8ce938a34a2a81d210c27bfadfe51874d8644a7d
salnguye <salnguyen95@gmail.com>
2021-07-19 08:02:58 -0500

    Create findfiles.py
    Pushing findingfiles.py script to main

---
+++ findfiles.py
@@ -0,0 +1,25 @@
+#sally.nguyen@oracle.com 7/16/21
+import pandas as pd
+import xml.etree.ElementTree as ET
+import os
+import re
+import csv
+pattern='^(\/[\w-]+)+\.|[\\.\w-]+\.(png|jpg|pdf|htm|html|mht|js)'
+columns=['Path', 'prev_src']
+df=pd.DataFrame(columns=columns)
+catalog = "/Users/salnguyen/Documents/content/catalog/root/shared"
+for root, dirs, files in os.walk(catalog, topdown=True):
+    for f in files:
+        webcat=os.path.join(root,f)
+        if(not webcat.endswith('.atr')):
+            with open(webcat, 'rb') as f:
+                content=f.readlines()
+                content=str(content)
+            if(re.search(pattern,content)):
+                matched=re.search(pattern,content)
+                #if matched:
+                prev_path=matched.group(0)
+                df=df.append({'Path':webcat,'prev_src':prev_path},ignore_index=True)
+                print(df)
+                #df.to_csv('BIITSEXTOAC_IRFiles.csv')
+
